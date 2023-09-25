d7282641d691a0b2e263da441bb77f96946fed46
TheGreatestbug <izyspikes@gmail.com>
2023-09-26 00:12:33 +0100

    Selection Sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Selects, duhh...
+ * @array: Array to be sorted
+ * @size: size of array
+ *
+ * Description: A function that sorts an array Selection sort style.
+ *
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t index, temp, jndex, min;
+
+	for (index = 0; index < size - 1; index++)
+	{
+		min = index;
+
+		for (jndex = index + 1; jndex < size; jndex++)
+		{
+			if (array[jndex] < array[min])
+			{
+				min = jndex;
+			}
+		}
+
+		if (min != index)
+		{
+			temp = array[index];
+			array[index] = array[min];
+			array[min] = temp;
+		}
+
+		print_array(array, size);
+	}
+}
