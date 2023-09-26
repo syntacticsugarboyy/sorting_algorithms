bf02dd766e4a622b62da0ddb33ab66d7a0990b81
TheGreatestbug <izyspikes@gmail.com>
2023-09-26 01:03:03 +0100

    Header File

--- sort.h
+++ sort.h
@@ -24,5 +24,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+size_t sep(int **array, size_t bottom, size_t top, size_t size);
+void quick_repeat(int **array, size_t bottom, size_t top, size_t size);
+
 
 #endif
